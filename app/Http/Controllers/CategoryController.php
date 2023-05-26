a5c533911b0c08d102b27ba1576bb578d13be2d5
mohamed el azzouzi <med.elazzouzi.97@gmail.com>
2023-05-26 10:12:45 +0100

    category order by

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
     public function index()
     {
         //
-        $categories = Category::all();
+        $categories = Category::orderBy('category_order')->get();
         return view('categories.index',compact('categories'));
     }
 
